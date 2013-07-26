73a70201ab33c6da11579552e4efeb6c74123eb3
root <root@bt.foo.org>
2013-07-26 22:49:07 +0700

    ngetes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ed1a4f6cca130d51edd817961ce417048967f15f8bb5be0cd6dd15190f333377b497367315bb1fa09ddb3e338c71b5abdce3f66c9d6c26becd85446f7c318ec4'
